6e48f8d0d5da175ccf72580df585a8ce0c2a5431
L4R424 <125917142+L4R424@users.noreply.github.com>
2023-05-23 14:40:53 -0700

    Update page.tsx

--- final/app/page.tsx
+++ final/app/page.tsx
@@ -1,5 +1,35 @@
-import React from "react";
-const Page = () =>{
-return <div>Page</div>
+"use client"
+import { useState } from "react"
+
+
+
+
+const Page = () => {
+  const [nombre,setNombre] = useState('Jesus Garcia Lara');
+  const [contador, setContador] = useState(0)
+
+  function contar(){
+    setContador(contador + 1);
+  }
+
+  function limpiar(){
+    setContador(0)
+  }
+
+  return (
+    <div>
+      <h1>Proyecto Final</h1>
+      <h2>Estructuras de Datos</h2>
+      <h3>{nombre}</h3>
+      <button onClick={contar}>
+        Contar
+      </button>
+      <p>{contador} numero de clics</p>
+      <button onClick={limpiar}>
+        Borrar
+      </button>
+    </div>
+  )
 }
+
 export default Page
